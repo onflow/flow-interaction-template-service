088081bce80ccaaa65af83b97a05df9ad3b5ef69
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2023-10-18 15:35:13 -0700

    Adds messages columns to template model

--- api/src/models/template.ts
+++ api/src/models/template.ts
@@ -5,6 +5,8 @@ class Template extends BaseModel {
   json_string!: string;
   testnet_cadence_ast_sha3_256_hash!: string;
   mainnet_cadence_ast_sha3_256_hash!: string;
+  messages_title_enUS?: string;
+  messages_description_enUS?: string;
 
   static get tableName() {
     return "templates";
