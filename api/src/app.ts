7fd647ddf84267e676027e12634814acc9c78da4
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2023-10-18 15:37:17 -0700

    Use versioned template routes

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,11 +3,13 @@ import cors from "cors";
 import express, { Request, Response } from "express";
 import "express-async-errors";
 import path from "path";
-import templateRouter from "./routes/template";
+import templateRouterV1 from "./routes/template-v1";
+import templateRouterV2 from "./routes/template-v2";
 import auditorsRouter from "./routes/auditors";
 import { TemplateService } from "./services/template";
 
 const V1 = "/v1/";
+const V2 = "/v2/";
 
 const corsOptions = {
   origin: "*",
@@ -26,7 +28,8 @@ const initApp = (
   app.use(cors(corsOptions));
   app.use(json());
   app.use(urlencoded({ extended: false }));
-  app.use(V1, templateRouter(templateService, namesJSONFile));
+  app.use(V1, templateRouterV1(templateService, namesJSONFile));
+  app.use(V2, templateRouterV2(templateService, namesJSONFile));
   app.use(V1, auditorsRouter(auditorsJSONFile));
 
   app.all("*", async (req: Request, res: Response) => {
